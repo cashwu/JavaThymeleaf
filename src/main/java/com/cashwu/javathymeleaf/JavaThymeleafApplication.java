b9e7613d7b549b7325702dd0269da08800450aff
Cash <cashwugeek@icloud.com>
2024-04-30 16:55:19 +0800

    init

---
+++ src/main/java/com/cashwu/javathymeleaf/JavaThymeleafApplication.java
@@ -0,0 +1,13 @@
+package com.cashwu.javathymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaThymeleafApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaThymeleafApplication.class, args);
+    }
+
+}
